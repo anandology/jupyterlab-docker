cabb6157be8317e39155ff015aed3947bda7d057
Anand Chitipothu <anandology@gmail.com>
2022-03-22 01:24:48 +0000

    Running as user root to avoid permission issues when mounting volumes.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM jupyter/base-notebook
 ADD jupyterlab.sh /usr/local/bin
+USER root
+WORKDIR /notebooks
 CMD ["jupyterlab.sh"]
--- jupyterlab.sh
+++ jupyterlab.sh
@@ -2,6 +2,7 @@
 
 TOKEN=${JUPYTERLAB_TOKEN:-jupyter}
 
-export NOTEBOOK_ARGS="--NotebookApp.token=$TOKEN"
-exec start-notebook.sh "$@"
+export NOTEBOOK_ARGS="--NotebookApp.token=$TOKEN --notebook-dir=/notebooks --LabApp.allow_root=True"
+exec jupyter lab $NOTEBOOK_ARGS "$@"
+
 
